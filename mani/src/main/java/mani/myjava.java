12b08afbe6687277a60dd2992c77b850f7618170
acer <acer@DESKTOP-F4207EA>
2022-11-13 20:21:11 +0530

    myjava first commit

---
+++ mani/src/main/java/mani/myjava.java
@@ -0,0 +1,13 @@
+package mani;
+
+public class myjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i want to become devops engineer");
+		System.out.println("God will help me ");
+
+
+	}
+
+}
