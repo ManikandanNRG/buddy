16d5af260d2fb83b17d8acdba3b0661cb3aad77a
manikandannrg <102239709+ManikandanNRG@users.noreply.github.com>
2022-12-11 13:10:57 +0530

    edited the java

--- mani/src/main/java/mani/myjava.java
+++ mani/src/main/java/mani/myjava.java
@@ -4,8 +4,8 @@ public class myjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i want to become devops engineer");
-		System.out.println("God will help me ");
+		System.out.println(" become devops engineer");
+		System.out.println("lets have a coffee");
 
 
 	}
